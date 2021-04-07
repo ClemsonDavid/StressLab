70db4e14124fd0263909829158f4483f8b8f0f56
jbaugh17 <55162842+jbaugh17@users.noreply.github.com>
2021-04-07 10:05:03 -0400

    Started working on the main page with fragments. I used The Band Database Starter example project as a guide.

--- StressApp/app/src/main/java/com/example/stressapp/MainActivity.java
+++ StressApp/app/src/main/java/com/example/stressapp/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.stressapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = new MainFragment();
+            fragmentManager.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
     }
 }
---
+++ StressApp/app/src/main/java/com/example/stressapp/MainFragment.java
@@ -0,0 +1,25 @@
+package com.example.stressapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import androidx.fragment.app.Fragment;
+import java.util.List;
+
+public class MainFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        LinearLayout layout = (LinearLayout) view;
+        return view;
+
+    }
+
+}
